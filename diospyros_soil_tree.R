e082ead3062ccf8ae0b69bd0da238e1640a3b21d
Katie Emelianova <katieemelianova@Katies-MacBook-Air.local>
2024-01-26 13:25:08 +0100

    start mucking baout with annotating tree

--- diospyros_soil_tree.R
+++ diospyros_soil_tree.R
@@ -47,15 +47,42 @@ species <- sub("[A-Za-z]$", "", nwk$tip.label) %>% str_sub(1, - 5)
 nwk$sample_id<-sample_id
 nwk$species<-species
 
-nwk<-root(nwk, which(nwk$tip.label == "sandwicensisG0000"))
+nwk<-root(nwk, which(nwk$tip.label == "sandwicensisG"))
 
+#nwk$tip.label <- nwk$sample_id
 
 dd <- data.frame(taxa=nwk$tip.label)
-p<-ggtree(nwk, size=2.3)
-p <- p %<+% dd
-
-p + geom_tiplab(size=3)
-
+circ<-ggtree(nwk, size=2.3, layout = "circular")
+circ <- circ %<+% dd
+circ + geom_tiplab(size=3)
 
 
+test<-leaf_soil_species %>% dplyr::select(demandeur, Mg_leaf, Ca_soil, Mg_soil, Na_soil, K_soil, P_soil, Al_soil, Fe_soil, Mn_soil, Cr_soil, Co_soil, Ni_soil, Cu_soil, Cd_soil, Mo_soil, Pb_soil, Zn_soil, S_soil)
+test %>% filter(demandeur %in% nwk$sample_id)
 
+
+
+test<-inner_join(test, data.frame(sample_id=nwk$sample_id,
+                            species = nwk$tip.label), by=c("demandeur"="sample_id"), multiple = "all")
+
+
+test<-column_to_rownames(test, var="species")
+
+gheatmap(circ + geom_tiplab(size=3), test[, c("Mg_leaf", "Ca_soil", "Mg_soil", "Na_soil", "K_soil", "P_soil", "Al_soil", "Fe_soil", "Mn_soil", "Cr_soil", "Co_soil", "Ni_soil", "Cu_soil", "Cd_soil", "Mo_soil", "Pb_soil", "Zn_soil", "S_soil"), drop=F], offset=.0001, width=.1,
+         colnames_angle=90, colnames_offset_y = 0.015)
+
+gheatmap(circ + geom_tiplab(size=3), test[, c("Mg_leaf", "Ca_soil", "Mg_soil", "Na_soil", "K_soil", "P_soil", "Al_soil", "Fe_soil"), drop=F], offset=.025, width=.5,
+         colnames_angle=90, colnames_offset_y = .15)
+
+gheatmap(circ + geom_tiplab(size=3), test[, c("Mn_soil", "Cr_soil", "Co_soil", "Ni_soil", "Cu_soil", "Cd_soil", "Mo_soil"), drop=F], offset=.0001, width=.5,
+         colnames_angle=90, colnames_offset_y = .15)
+
+gheatmap(circ + geom_tiplab(size=3), test[, c("Mn_soil", "Ni_soil", "Cr_soil"), drop=F], offset=.0001, width=.5,
+         colnames_angle=90, colnames_offset_y = .15)
+
+
+
+
+
+
+circ$data$label %in% rownames(test)
